0bfd77c2b713730bbc39f05975cf58fc292a6e72
Luc Patiny <luc@patiny.com>
2018-01-10 17:44:23 +0100

    fix eslint

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,23 +1,23 @@
 import sum from '..';
 
 test('test sum', () => {
-    let a={
-        A:1,
-        B:2,
-        C:3
-    }
-    let b={
-        B:3,
-        C:4,
-        D:5
-    }
-    let result=sum(a,b);
+    let a = {
+        A: 1,
+        B: 2,
+        C: 3
+    };
+    let b = {
+        B: 3,
+        C: 4,
+        D: 5
+    };
+    let result = sum(a, b);
     expect(a).toBe(result);
     expect(a).toEqual({
-        A:1,
-        B:5,
-        C:7,
-        D:5
-    })
-    
+        A: 1,
+        B: 5,
+        C: 7,
+        D: 5
+    });
+
 });
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 /**
  * Modify object a to join it with b and make the sum of each of the keys
- * @param {*} a 
- * @param {*} b 
- * 
+ * @param {*} a
+ * @param {*} b
+ *
  * @return {object}
  */
- 
-export default function sum(a,b) {
+
+export default function sum(a, b) {
     for (var key of Object.keys(b)) {
         if (a[key]) {
-            a[key]+=b[key];
+            a[key] += b[key];
         } else {
-            a[key]=b[key];
+            a[key] = b[key];
         }
     }
     return a;
